fb25cff6beaa9276e409977d679ff9fc8571a739
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-05-24 21:00:48 +0530

    navigation setup in catalog

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import {Outlet} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function Catalog() {
+
+  const navigate = useNavigate();
+  function handleNavigate(type) {
+    type === "spacecrafts" ? navigate("/catalog/spacecrafts") : navigate("/catalog/commercial");
+  }
+
   return (
     <div>Catalog
-      <Outlet/>
+      
+        <button onClick={() => handleNavigate("spacecrafts")}>Spacecrafts link</button>
+        <button onClick={() => handleNavigate("commercial")}>Commercial link</button>
+      
     </div>
   )
 }
