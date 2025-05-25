54e13d85aacceaf37765601cb311d1e70e689e7b
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-05-25 14:43:26 +0530

    centers data retrieval

--- src/Pages/Centers.jsx
+++ src/Pages/Centers.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useEffect,useState } from 'react';
+import {useGetCentersQuery} from '../Redux/Services/isroVercelApi';
 
 function Centers() {
+  const {data: centers, isLoading, isError} = useGetCentersQuery();
+  const [displayCenters, setDisplayCenters] = useState([]);
+  useEffect(() => {
+    if (centers) {
+      setDisplayCenters(centers.centres);
+    }
+  }, [centers]);
   return (
-    <div>Centers</div>
+    <div>Centers
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error loading data</div>}
+      {displayCenters && (
+        <ul className="list-disc pl-5">
+          {displayCenters.map((center, index) => (
+            <li key={index} className="mb-2">
+              <h2 className="font-bold">{center.name}</h2>
+              <p>{center.Place}</p>
+              <p>{center.State}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 
