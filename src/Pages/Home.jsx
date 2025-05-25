75fa724d7762ccd094200a3776cad32b7fc11705
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-05-26 00:20:02 +0530

    Spline component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import SplineScene from '../Components/SplineScene'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+      <SplineScene/>
+    </div>
   )
 }
 
