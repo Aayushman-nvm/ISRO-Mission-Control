20ecf52d4296571de2bc849b7aaf8b8dd4eef12a
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-05-25 15:14:32 +0530

    Navigation and Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Footer() {
+  const resourceArr = [{
+    link: "https://www.isro.gov.in/",
+    name: "ISRO"
+  },{
+    link:"https://isro.vercel.app/",
+    name: "ISRO Vercel API source"
+  },{
+    link:"https://isrostats.in/home",
+    name: "ISRO Statistics API source"
+  }, {
+    link: "https://services.isrostats.in/api/launches",
+    name: "Launchers API"
+  }, {
+    link: "https://services.isrostats.in/api/spacecraft",
+    name: "Spacecrafts API"
+  }, {
+    link: "https://isro.vercel.app/api/customer_satellites",
+    name: "customer satellites API"
+  }, {
+    link: "https://isro.vercel.app/api/centres",
+    name: "Centers API"
+  }];
   return (
-    <div>Footer</div>
+    <div>Footer
+      {resourceArr.map((item, index) => (
+        <NavLink key={index} to={item.link} target="_blank" rel="noopener noreferrer" className={({ isActive }) => isActive ? "active" : "inactive"}>
+          {item.name}
+        </NavLink>
+      ))}
+    </div>
   )
 }
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function NavBar() {
+  const linkArr = [{
+    link: "/",
+    name: "Home"
+  }, {
+    link: "/catalog",
+    name: "Catalog"
+  }, {
+    link: "/centers",
+    name: "Centers"
+  }, {
+    link: "/explore",
+    name: "Explore"
+  }, {
+    link: "/dashboard",
+    name: "Dashboard"
+  }];
   return (
-    <div>NavBar</div>
+    <div>NavBar
+      {linkArr.map((item, index) => (
+        <NavLink key={index} to={item.link} className={({ isActive }) => isActive ? "active" : "inactive"}>
+          {item.name}
+        </NavLink>
+      ))}
+    </div>
   )
 }
 
