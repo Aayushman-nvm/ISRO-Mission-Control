3da8dea645b82796cf6dd56d571e737f0cd13bec
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-05-30 21:40:23 +0530

    improved card color

--- src/Components/CommercialCard.jsx
+++ src/Components/CommercialCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function CommercialCard({ id, country, launchDate, mass, launcher }) {
   return (
-    <div className="bg-gray-900 text-white p-4 rounded-xl shadow-md hover:shadow-lg transition duration-300">
+    <div className="bg-gradient-to-r from-[#0a0a1a] via-transparent to-gray-900/80 text-white p-4 rounded-xl shadow-md hover:shadow-lg transition duration-300">
       <h2 className="text-lg font-semibold mb-2">Launch ID: {id}</h2>
       <p className="text-sm"><span className="font-medium">Date:</span> {launchDate}</p>
       <p className="text-sm"><span className="font-medium">Country:</span> {country}</p>
